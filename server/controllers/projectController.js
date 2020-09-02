9e6068edf699b08eeabcbd23a2f54235cb191f3c
Sachin Tekchandani <sachin6291@gmail.com>
2020-09-02 23:58:40 +0100

    Mern task: server project update commit:2

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,5 +1,14 @@
 const Project = require('../models/Project')
+const {validationResult} = require('express-validator')
+
 exports.createProject = async(req,res)=>{
+
+  //check if there are errors in projects
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
   try {
     //create a new project
     const project = new Project(req.body);
@@ -10,9 +19,49 @@ exports.createProject = async(req,res)=>{
     //save the project
     project.save()
     res.json(project)
-    
+
   } catch (error) {
     console.log(error);
-    res.status(500).send('There has been an error')
+    res.status(500).send('There has been an error in createProject')
   }
 } 
+
+//get all the projects of the current user
+exports.getProjects = async(req, res) =>{
+  try {
+    const projects = await Project.find({user: req.user.id}).sort({registry:-1})
+    res.json({projects})
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('There has been an error in getProjects')
+  }
+}
+// update a project
+exports.updateProject = async(req,res)=>{
+  
+  //check if there are errors in user
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  //extract info of project
+  const{name}= req.body
+  const newProject ={}
+  
+  if(name){
+    newProject.name = name
+  }
+
+  try {
+    //check the id
+    let project = Project.findById(req.params.id);
+    //check if project exists
+    //verify the creator of the project
+    //update
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('There has been an error in updateProject')
+  }
+}
--- server/routes/projects.js
+++ server/routes/projects.js
@@ -2,14 +2,28 @@ const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController')
 const auth = require('../middleware/auth')
+const {check}= require('express-validator')
 
 //create a project : api/projects
 router.post('/',
   auth,
+  [
+    check('name','Project name is Required').trim().notEmpty()
+  ],
   projectController.createProject
 )
+//get all the projects
 router.get('/',
   auth,
-  projectController.createProject
+  projectController.getProjects
+)
+
+//update the project through the id
+router.put('/:id',
+  auth,
+  [
+    check('name', 'Project name is Required in update').trim().notEmpty()
+  ],
+  projectController.updateProject
 )
 module.exports = router;
